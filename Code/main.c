4b276300712a7945978af8f610f911ff388590e8
Miguel Vera <miguel.vera7908@gmail.com>
2020-05-09 11:34:00 -0600

    Added main.c

---
+++ Code/main.c
@@ -0,0 +1,14 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+extern	int sonic();
+
+//Define Pins
+#define R_PIN               5
+#define G_PIN               3
+
+//Define Led Pin controls
+#define R_ON                (PORTB |= (1<<R_PIN))
+#define R_OFF               (PORTB &= ~(1<<R_PIN))
+#define G_ON                (PORTB |= (1<<G_PIN))
+#define G_OFF               (PORTB &= ~(1<<G_PIN))
